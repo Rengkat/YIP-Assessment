beff0a31e909ed41a26efc4b320342f1f4723e55
Rengkat <successinmotion11@gmail.com>
2025-02-18 21:16:19 +0100

    importation of components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,21 @@ import CustomerForm from "./Components/CustomerForm";
 import CustomerList from "./Components/CustomerList";
 
 const App = () => {
-  return <div className="flex flex-col md:flex-row h-screen"></div>;
+  return (
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="flex-1">
+        <MapSection />
+      </div>
+
+      <CustomerForm />
+
+      <CustomerForm />
+
+      <div className="w-full md:w-64 p-4 bg-white shadow-lg">
+        <CustomerList />
+      </div>
+    </div>
+  );
 };
 
 export default App;
